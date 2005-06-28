d073ab9709249987423bcf4c3ec9d37649458bd5
baijum81 <baijum81@bb3ae673-f1ec-0310-bf2f-cd7a7298aca9>
2005-06-28 06:01:14 +0000

    * baiju: * baiju/culebra: * baiju/culebra/unit: * baiju/culebra/unit/__init__.py: * baiju/culebra/plugins: * baiju/culebra/plugins/__init__.py: * baiju/culebra/image: * baiju/culebra/gui: * baiju/culebra/gui/__init__.py: * baiju/culebra/gui/main.py: * baiju/culebra/edit: * baiju/culebra/edit/__init__.py: * baiju/culebra/debug: * baiju/culebra/debug/__init__.py: * baiju/culebra/docs: * baiju/culebra/misc: * baiju/culebra/README: * baiju/culebra/culib: * baiju/culebra/culib/__init__.py: * baiju/culebra/tests: * baiju/culebra/culebra.py: * baiju/culebra/refactor: * baiju/culebra/refactor/__init__.py: * baiju/culebra/TODO: * baiju/culebra/COPYING: * baiju/culebra/glade: * baiju/culebra/glade/main.glade: * baiju/culebra/NEWS: * baiju/culebra/culebra:
    This branch is created from temporary development.

---
+++ baiju/culebra/culebra.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""\
+Culebra -- A Python IDE
+
+Under development.
+
+"""
+
+__revision__ = '0.1.1'
+
+import gtk
+
+from gui import main
+
+win = main.MainWindow()
+
+#win.show_window()
+gtk.main()
+
